ea140a636d5494abed2c164ab6458ba1142185c1
bmthd <81406938+bmthd@users.noreply.github.com>
2024-05-19 04:52:12 +0900

    Develop (#14)
    * Feature/add-components (#9)
    
    * add:some components
    
    * update:For simplicity, dropped the use of numbers.
    
    * fix:lint
    
    * update:version
    
    * update:Context name (#13)
    
    * update:Context name (#13)

--- lib/context.tsx
+++ lib/context.tsx
@@ -7,10 +7,8 @@ const headingLevels = ["h1", "h2", "h3", "h4", "h5", "h6"] as const;
 /** String literal type for heading levels */
 export type HeadingLevel = (typeof headingLevels)[number];
 
-const getNextLevel = (level: HeadingLevel): HeadingLevel => {
-  const index = headingLevels.indexOf(level);
-  return headingLevels[index + 1] || level;
-};
+const getNextLevel = (level: HeadingLevel): HeadingLevel =>
+  headingLevels[headingLevels.indexOf(level) + 1] || level;
 
 const HeadingLevelContext = createContext<HeadingLevel>("h1");
 
@@ -30,7 +28,7 @@ export const useNextLevel = () => {
 /**
  * Provider for the next heading level.
  */
-export const NextHeadingLevelProvider: FC<{ children: ReactNode }> = ({ children }) => {
+export const NextHeadingProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const level = useNextLevel();
   return <HeadingLevelContext.Provider value={level}>{children}</HeadingLevelContext.Provider>;
 };
@@ -48,19 +46,19 @@ const Case = {
 >;
 
 /**
- * Renders the heading component according to the number of times the `NextHeadingLevelProvider` is nested.
+ * Renders the heading component according to the number of times the `NextHeadingProvider` is nested.
  *
  * usage:
  * ```tsx
  * export const Page: React.FC = () => (
  *  <>
  *   <H>Heading</H> // this will render <h1>Heading</h1>
- *   <NextHeadingLevelProvider>
+ *   <NextHeadingProvider>
  *    <H>Heading2</H> // this will render <h2>Heading2</h2>
- *    <NextHeadingLevelProvider>
+ *    <NextHeadingProvider>
  *     <H>Heading3</H> // this will render <h3>Heading3</h3>
- *    </NextHeadingLevelProvider>
- *   </NextHeadingLevelProvider>
+ *    </NextHeadingProvider>
+ *   </NextHeadingProvider>
  *  </>
  * )
  * ```
