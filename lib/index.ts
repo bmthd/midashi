d3012dff8f5714e43a8b50f353f40668e67cdff3
じょうげん <81406938+bmthd@users.noreply.github.com>
2024-05-27 22:06:45 +0900

    fix:external-dependencies (#16)
    * fix:external-dependencies
    
    * chore:update readme
    
    * refactor:re-export
    
    * chore:change version

--- lib/index.ts
+++ lib/index.ts
@@ -1,23 +1,8 @@
-import { Main, Section, Article, Aside, Footer, Header, Nav } from "./components";
-import {
+export { Main, Section, Article, Aside, Footer, Header, Nav } from "./components";
+export {
   H,
   NextHeadingProvider,
   useCurrentLevel,
   useNextLevel,
   type HeadingLevel,
 } from "./context";
-
-export {
-  Main,
-  Section,
-  Article,
-  Aside,
-  Footer,
-  Header,
-  Nav,
-  H,
-  NextHeadingProvider,
-  useCurrentLevel,
-  useNextLevel,
-  type HeadingLevel,
-};
